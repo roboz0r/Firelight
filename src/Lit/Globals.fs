57f495a91127d0dc21bd9f2b7378af5f4329d312
Rob Lenders <mental.blindfolds.on@gmail.com>
2024-09-22 14:41:30 -0500

    Add Symbol API

--- src/Lit/Globals.fs
+++ src/Lit/Globals.fs
@@ -15,3 +15,45 @@ type Number = interface end
 
 [<Global>]
 type TemplateStringsArray = interface end
+
+/// <summary>
+/// A unique identifier.
+/// </summary>
+/// <seealso href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol" />
+[<Global>]
+type symbol =
+    /// Expose the [[Description]] internal slot of a symbol directly.
+    abstract description: string option
+
+[<Erase>]
+type JS =
+    /// Returns a new, unique Symbol value.
+    [<Global>]
+    static member inline Symbol() : symbol = nativeOnly
+
+    /// Returns a new, unique Symbol value.
+    [<Global>]
+    static member inline Symbol(description: string) : symbol = nativeOnly
+
+    /// Returns a new, unique Symbol value.
+    [<Global>]
+    static member inline Symbol(description: float) : symbol = nativeOnly
+
+[<Erase>]
+module JS =
+    [<Global>]
+    type Symbol =
+        /// <summary>
+        /// Returns a Symbol object from the global symbol registry matching the given key if found.
+        /// Otherwise, returns a new symbol with this key.
+        /// </summary>
+        /// <param name="key">key to search for.</param>
+        [<CompiledName("for")>]
+        static member inline for'(description: string) : symbol = nativeOnly
+
+        /// <summary>
+        /// Returns a key from the global symbol registry matching the given Symbol if found.
+        /// Otherwise, returns a undefined.
+        /// </summary>
+        /// <param name="sym">Symbol to find the key for.</param>
+        static member inline keyFor(sym: symbol) : string option = nativeOnly
